c7c911c0b044a3c0fdffff846f154ccd3035aca4
Gelan Villanueva <gelanvillanueva21@gmail.com>
2026-08-22 13:14:14 +0800

    Finished <[config]>

---
+++ backend/app/config/config.py
@@ -0,0 +1,30 @@
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class DatabaseSettings(BaseSettings):
+    DATABASE_URL: str
+
+
+class AuthSettings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str='HS256'
+    ACCESS_TOKEN_EXPIRE_HOURS: 12
+
+
+class Settings(DatabaseSettings, AuthSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra='ignore'
+    )
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
+
+
