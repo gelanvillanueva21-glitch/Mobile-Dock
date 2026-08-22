497504b8f16eea910bc32725cd1647ffa7da41fe
Gelan Villanueva <gelanvillanueva21@gmail.com>
2026-08-22 22:49:03 +0800

    Added alembic

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -10,7 +10,7 @@ class DatabaseSettings(BaseSettings):
 class AuthSettings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str='HS256'
-    ACCESS_TOKEN_EXPIRE_HOURS: 12
+    ACCESS_TOKEN_EXPIRE_HOURS: int = 12
 
 
 class Settings(DatabaseSettings, AuthSettings):
